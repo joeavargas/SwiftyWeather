36fc39574da29842fc29b630d20e284086550085
Joe Vargas <joeavargas@outlook.com>
2021-09-22 04:55:35 -0500

    import skeloton project

---
+++ SwiftyWeather/SwiftyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWeatherApp.swift
+//  SwiftyWeather
+//
+//  Created by Joe Vargas on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
